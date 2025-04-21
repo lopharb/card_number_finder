2fd106301cbc7ceef9cb71f7fb0a5beb559ac72d
Ivan Shkel <skillofivan@gmail.com>
2025-04-21 19:31:27 +0300

    [yolo_detections] simplified implementation for MLFlow

--- training/mlflow_trainer.py
+++ training/mlflow_trainer.py
@@ -1,56 +1,16 @@
-import os
-from pathlib import Path
-import yaml
-
-from ultralytics.models.yolo.segment import SegmentationTrainer
-import mlflow
-from ultralytics.utils import yaml_load
-
-from .card_dataset import CardSegmentationDataset
-
 from ultralytics.models.yolo.segment import SegmentationTrainer
 import mlflow
 
 
 class MLflowSegmentationTrainer(SegmentationTrainer):
-    def __init__(self, overrides=None, _callbacks=None):
-        # Call the original constructor (DO NOT block cfg loading)
-        super().__init__(overrides=overrides, _callbacks=_callbacks)
-        if isinstance(self.args.data, str):
-            data_path = os.path.abspath(self.args.data)
-            self.data = yaml_load(data_path)  # now self.data is guaranteed to exist
-            print('loading yaml...')
-        else:
-            self.data = self.args.data  # already a dict
-        print(self.data)
+    """
+    A class extending the SegmentationTrainer class to add MLFlow tracking.
+    """
 
-    def _get_paths(self, data_cfg_path):
-        data_cfg_path_ = Path(data_cfg_path)
-        with open(data_cfg_path_, "r") as f:
-            data_cfg = yaml.safe_load(f)
-
-        train_path = data_cfg_path_.parent / data_cfg["train"]
-        val_path = data_cfg_path_.parent / data_cfg["val"]
-        return train_path.parent, val_path.parent
-
-    def get_dataset(self):
+    def before_train(self) -> None:
         """
-        Override this only if you want custom dataset loading (like our on-the-fly augmentations).
-        Otherwise, just use default and let YOLO handle it.
+        Log MLFlow params here (after everything is initialized properly)
         """
-        train_path, val_path = self._get_paths(self.args.data)
-        trainset = CardSegmentationDataset(
-            image_dir=os.path.join(train_path, 'images'),
-            label_dir=os.path.join(train_path, 'labels')
-        )
-        valset = CardSegmentationDataset(
-            image_dir=os.path.join(val_path, 'images'),
-            label_dir=os.path.join(val_path, 'labels')
-        )
-        return trainset, valset
-
-    def before_train(self):
-        # Log MLFlow params here (after everything is initialized properly)
         mlflow.set_experiment("yolo-segmentation")
         mlflow.log_params({
             "epochs": self.args.epochs,
